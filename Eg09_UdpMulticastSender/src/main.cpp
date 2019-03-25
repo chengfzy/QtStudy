44fb08ebb6b18b097ad7079cad0dea88213e8b44
Jeffery Cheng <chengcheng2006@163.com>
2019-03-25 23:48:00 +0800

    add print network card support multicast info

--- Eg09_UdpMulticastSender/src/main.cpp
+++ Eg09_UdpMulticastSender/src/main.cpp
@@ -2,6 +2,18 @@
 #include "Sender.h"
 
 int main(int argc, char* argv[]) {
+    // find network card support multicast
+    QList<QNetworkInterface> list = QNetworkInterface::allInterfaces();
+    for (auto& v : list) {
+        qDebug() << v.name() << " " << v.flags();
+
+        QList<QNetworkAddressEntry> listAddrs = v.addressEntries();
+        for (auto& addr : listAddrs) {
+            qDebug() << addr.ip().toString();
+        }
+        qDebug() << "========================";
+    }
+
     QApplication app(argc, argv);
     Sender sender;
     sender.show();
